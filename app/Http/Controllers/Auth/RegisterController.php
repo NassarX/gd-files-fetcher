85bcd8644addaad5a6fc3b446a17867e9692f32c
Mahmoud Nassar <m.hamdy1994@gmail.com>
2018-04-11 19:32:41 +0200

    Integrate `Laravel-Socialite` to authenticate with Google OAuth provider.
    # Update register functionality to accept google-auth registeration & authentication.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Google_Service_Drive;
 
 class RegisterController extends Controller
 {
@@ -50,8 +53,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
         ]);
     }
 
@@ -63,10 +66,61 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+    	$createdUser = User::updateOrCreate(
+    		['email' => $data['email']],
+		    [
+	        	'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+	        ]
+        );
+
+    	if ($createdUser) {
+		    $createdUser->tokens->isEmpty()? $createdUser->tokens()->create($data)
+			    : $createdUser->tokens->first()->update($data);
+	    }
+
+	    return $createdUser;
     }
+
+	/**
+	 * Redirect the user to the Google authentication page.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function redirectToProvider()
+	{
+		return Socialite::driver('google')
+		                ->scopes(['openid', 'profile', 'email', Google_Service_Drive::DRIVE_READONLY])
+		                ->redirect();
+	}
+
+	/**
+	 * Obtain the user information from Google+.
+	 *
+	 * @param $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function handleProviderCallback(Request $request)
+	{
+		$user = Socialite::driver('google')->user();
+
+		// Set token for the Google API PHP Client
+		$google_client_token = [
+			'access_token' => $user->token,
+			'refresh_token' => $user->refreshToken,
+			'expires_in' => $user->expiresIn
+		];
+
+		// Add user info on the fly inside a request instance
+		$request->request->add(
+			array_merge([
+				'name' => $user->name,
+				'email' => $user->email,
+				'password' => str_random(8)
+			], $google_client_token)
+		);
+
+		return $this->register($request);
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('auth/google', 'Auth\RegisterController@redirectToProvider')->name('auth.google');
+Route::get('auth/google/callback', 'Auth\RegisterController@handleProviderCallback')->name('auth.google.callback');
