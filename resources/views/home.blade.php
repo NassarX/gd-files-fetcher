8ce91ee8983f218a73b2cd53357c2fd87fb2ea7b
Mahmoud Nassar <m.hamdy1994@gmail.com>
2018-04-11 23:13:59 +0200

    Integrate simple GUI for listing user' files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,51 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">
+                    <form method="POST" action="{{ route('gd.files.fetch') }}">
+                        @csrf
+                        <button type="submit" class="btn-info"> Fetch Files </button>
+                    </form>
+                </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                <div class="card-body row">
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
                         </div>
                     @endif
 
-                    You are logged in!
+                    <table class="table table-striped table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Mime Type</th>
+                            <th>File Size</th>
+                            <th>Download Url</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @if(isset($userFiles))
+                            @foreach($userFiles as $file)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $file->title }}</td>
+                                    <td>{{ $file->mime_type }}</td>
+                                    <td>{{ $file->size }} {{ $file->size? '(KB)' : '---' }}</td>
+                                    <td> <a href="{{ $file->download_url }}">download url</a></td>
+                                </tr>
+                            @endforeach
+                        @endif
+                        </tbody>
+                    </table>
+
                 </div>
+                @if(isset($userFiles))
+                    {{ $userFiles->links() }}
+                @endif
             </div>
         </div>
     </div>
